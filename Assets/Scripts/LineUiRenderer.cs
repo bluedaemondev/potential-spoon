0808b97fca09cfaaa10645a03bca0b98cf43b5bc
Juan I. Lanosa <blackmoodgames@gmail.com>
2021-01-26 03:30:37 -0300

    alguito

---
+++ Assets/Scripts/LineUiRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UILibrary
+{
+    public class LineUiRenderer : MonoBehaviour
+    {
+        [SerializeField]
+        List<Transform> pointsToConnect = new List<Transform>();
+        LineRenderer lineRenderer;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
